e59cd46382b4d09a2c7a02323897fdb81719053b
4irw4lk3r <federico.delgado@gmail.com>
2019-01-14 23:53:27 -0200

    First commit backend

---
+++ backend/src/main/java/com/example/springbootvuejs/SpringbootVuejsApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootvuejs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVuejsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVuejsApplication.class, args);
+	}
+
+}
+
